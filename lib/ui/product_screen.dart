4504ed618da08d6c4ad4b58e1c66a22696bf585c
CodigoAlphaCol <47418849+codigoalphacol@users.noreply.github.com>
2019-03-28 19:56:38 -0500

    Update product_screen.dart
    Nuevo código modificado para agregar una imagen desde la galería o cámara la imagen

--- lib/ui/product_screen.dart
+++ lib/ui/product_screen.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_app_products/model/product.dart';
+//nuevo para imagenes
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:date_format/date_format.dart';
+
+File image;
+String filename;
 
 class ProductScreen extends StatefulWidget {
   final Product product;
@@ -20,6 +28,38 @@ class _ProductScreenState extends State<ProductScreen> {
   TextEditingController _descriptionController;
   TextEditingController _priceController;
   TextEditingController _stockController;
+  
+   //nuevo imagen
+  String productImage;
+
+  pickerCam() async {
+    File img = await ImagePicker.pickImage(source: ImageSource.camera);
+    // File img = await ImagePicker.pickImage(source: ImageSource.camera);
+    if (img != null) {
+      image = img;
+      setState(() {});
+    }
+  }
+
+  pickerGallery() async {
+    File img = await ImagePicker.pickImage(source: ImageSource.gallery);
+    // File img = await ImagePicker.pickImage(source: ImageSource.camera);
+    if (img != null) {
+      image = img;
+      setState(() {});
+    }
+  }
+  
+  Widget divider() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+      child: Container(
+        width: 0.8,
+        color: Colors.black,
+      ),
+    );
+  }
+  //fin nuevo imagen
 
   @override
   void initState() {
@@ -30,7 +70,8 @@ class _ProductScreenState extends State<ProductScreen> {
     _descriptionController = new TextEditingController(text: widget.product.description);
     _priceController = new TextEditingController(text: widget.product.price);
     _stockController = new TextEditingController(text: widget.product.stock);
-
+    productImage = widget.product.productImage;
+    print(productImage);
   }
 
   @override
@@ -38,80 +79,165 @@ class _ProductScreenState extends State<ProductScreen> {
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       appBar: AppBar(
-        title: Text('Products DB'),
-        backgroundColor: Colors.deepOrangeAccent,
+        title: Text('Products'),
+        backgroundColor: Colors.pinkAccent,
       ),
-      body: Container(
-        height: 570.0,
+      body: SingleChildScrollView(
+       
+        //height: 570.0,
         padding: const EdgeInsets.all(20.0),
         child: Card(
           child: Center(
             child: Column(
               children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    new Container(
+                      height: 100.0,
+                      width: 100.0,
+                      decoration: new BoxDecoration(
+                          border: new Border.all(color: Colors.blueAccent)),
+                      padding: new EdgeInsets.all(5.0),
+                      child: image == null ? Text('Add') : Image.file(image),                      
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 2.2),
+                      child: new Container(
+                        height: 100.0,
+                        width: 100.0,
+                        decoration: new BoxDecoration(
+                            border: new Border.all(color: Colors.blueAccent)),
+                        padding: new EdgeInsets.all(5.0),
+                        child: productImage == '' ? Text('Edit') : Image.network(productImage+'?alt=media'),
+                      ),
+                    ),
+                    Divider(),
+                    //nuevo para llamar imagen de la galeria o capturarla con la camara
+                    new IconButton(
+                        icon: new Icon(Icons.camera_alt), onPressed: pickerCam),
+                    Divider(),
+                    new IconButton(
+                        icon: new Icon(Icons.image), onPressed: pickerGallery),
+                  ],
+                ),
                 TextField(
                   controller: _nameController,
-                  style: TextStyle(fontSize: 17.0,color: Colors.deepOrangeAccent),
-                  decoration: InputDecoration(icon: Icon(Icons.person),
-                      labelText: 'Name'),
+                  style:
+                      TextStyle(fontSize: 17.0, color: Colors.deepOrangeAccent),
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.person), labelText: 'Name'),
                 ),
-                Padding(padding: EdgeInsets.only(top: 8.0),),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                ),
                 Divider(),
                 TextField(
                   controller: _codebarController,
-                  style: TextStyle(fontSize: 17.0,color: Colors.deepOrangeAccent),
-                  decoration: InputDecoration(icon: Icon(Icons.person),
-                      labelText: 'CodeBar'),
+                  style:
+                      TextStyle(fontSize: 17.0, color: Colors.deepOrangeAccent),
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.person), labelText: 'CodeBar'),
                 ),
-                Padding(padding: EdgeInsets.only(top: 8.0),),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                ),
                 Divider(),
                 TextField(
                   controller: _descriptionController,
-                  style: TextStyle(fontSize: 17.0,color: Colors.deepOrangeAccent),
-                  decoration: InputDecoration(icon: Icon(Icons.list),
-                      labelText: 'Description'),
+                  style:
+                      TextStyle(fontSize: 17.0, color: Colors.deepOrangeAccent),
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.list), labelText: 'Description'),
                 ),
-                Padding(padding: EdgeInsets.only(top: 8.0),),
+                Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                ),
                 Divider(),
                 TextField(
                   controller: _priceController,
-                  style: TextStyle(fontSize: 17.0,color: Colors.deepOrangeAccent),
-                  decoration: InputDecoration(icon: Icon(Icons.monetization_on),
-                      labelText: 'Price'),
+                  style:
+                      TextStyle(fontSize: 17.0, color: Colors.deepOrangeAccent),
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.monetization_on), labelText: 'Price'),
                 ),
-                Padding(padding: EdgeInsets.only(top: 8.0),),
+                Padding(
+                  padding: EdgeInsets.only(top: 1.0),
+                ),
                 Divider(),
                 TextField(
                   controller: _stockController,
-                  style: TextStyle(fontSize: 17.0,color: Colors.deepOrangeAccent),
-                  decoration: InputDecoration(icon: Icon(Icons.shop),
-                      labelText: 'Stock'),
+                  style:
+                      TextStyle(fontSize: 17.0, color: Colors.deepOrangeAccent),
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.shop), labelText: 'Stock'),
                 ),
-                Padding(padding: EdgeInsets.only(top: 8.0),),
+                Padding(
+                  padding: EdgeInsets.only(top: 1.0),
+                ),
                 Divider(),
-                FlatButton(onPressed: () {
-                  if (widget.product.id != null) {
-                    productReference.child(widget.product.id).set({
-                      'name': _nameController.text,
-                      'codebar': _codebarController.text,
-                      'description': _descriptionController.text,
-                      'price': _priceController.text,
-                      'stock': _stockController.text,
-                    }).then((_) {
-                      Navigator.pop(context);
-                    });
-                  } else {
-                    productReference.push().set({
-                      'name': _nameController.text,
-                      'codebar': _codebarController.text,
-                      'description': _descriptionController.text,
-                      'price': _priceController.text,
-                      'stock': _stockController.text,
-                    }).then((_) {
-                      Navigator.pop(context);
-                    });
-                  }
-                },
-                    child: (widget.product.id != null) ? Text('Update') : Text('Add')),
+                FlatButton(
+                    onPressed: () {
+                      //nuevo imagen
+                      if (widget.product.id != null) {
+                        var now = formatDate(
+                            new DateTime.now(), [yyyy, '-', mm, '-', dd]);
+                        var fullImageName =
+                            '${_nameController.text}-$now' + '.jpg';
+                        var fullImageName2 =
+                            '${_nameController.text}-$now' + '.jpg';
+
+                        final StorageReference ref =
+                            FirebaseStorage.instance.ref().child(fullImageName);
+                        final StorageUploadTask task = ref.putFile(image);
+
+                        var part1 =
+                            'https://firebasestorage.googleapis.com/v0/b/flutterimagen.appspot.com/o/';//esto cambia segun su firestore
+
+                        var fullPathImage = part1 + fullImageName2;
+
+                        productReference.child(widget.product.id).set({
+                          'name': _nameController.text,
+                          'codebar': _codebarController.text,
+                          'description': _descriptionController.text,
+                          'price': _priceController.text,
+                          'stock': _stockController.text,
+                          'ProductImage': '$fullPathImage'
+                        }).then((_) {
+                          Navigator.pop(context);
+                        });
+                      } else {
+                        //nuevo imagen
+                        var now = formatDate(
+                            new DateTime.now(), [yyyy, '-', mm, '-', dd]);
+                        var fullImageName =
+                            '${_nameController.text}-$now' + '.jpg';
+                        var fullImageName2 =
+                            '${_nameController.text}-$now' + '.jpg';
+
+                        final StorageReference ref =
+                            FirebaseStorage.instance.ref().child(fullImageName);
+                        final StorageUploadTask task = ref.putFile(image);
+
+                        var part1 =
+                            'https://firebasestorage.googleapis.com/v0/b/flutterimagen.appspot.com/o/'; //esto cambia segun su firestore                           
+
+                        var fullPathImage = part1 + fullImageName2;
+
+                        productReference.push().set({
+                          'name': _nameController.text,
+                          'codebar': _codebarController.text,
+                          'description': _descriptionController.text,
+                          'price': _priceController.text,
+                          'stock': _stockController.text,
+                          'ProductImage': '$fullPathImage'//nuevo imagen
+                        }).then((_) {
+                          Navigator.pop(context);
+                        });
+                      }
+                    },
+                    child: (widget.product.id != null)
+                        ? Text('Update')
+                        : Text('Add')),
               ],
             ),
           ),
