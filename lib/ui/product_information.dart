8584b5b1efe2e74229a13c4a6a410ea32da611e3
CodigoAlphaCol <47418849+codigoalphacol@users.noreply.github.com>
2019-03-28 19:58:34 -0500

    Update product_information.dart
    Con cambios para mostrar una imagen

--- lib/ui/product_information.dart
+++ lib/ui/product_information.dart
@@ -16,26 +16,29 @@ class _ProductInformationState extends State<ProductInformation> {
 
   List<Product> items;
 
+  String productImage;//nuevo
+
   @override
-  void initState() {
-    // TODO: implement initState
+  void initState() {   
     super.initState();
+    productImage = widget.product.productImage;//nuevo
+    print(productImage);//nuevo
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Product Information'),
-        backgroundColor: Colors.green,
+        title: Text('Product Information y Foto'),
+        backgroundColor: Colors.purpleAccent,
       ),
       body: Container(
-        height: 400.0,
+        height: 800.0,
         padding: const EdgeInsets.all(20.0),
         child: Card(
           child: Center(
             child: Column(
-              children: <Widget>[
+              children: <Widget>[                
                 new Text("Name : ${widget.product.name}", style: TextStyle(fontSize: 18.0),),
                 Padding(padding: EdgeInsets.only(top: 8.0),),
                 Divider(),
@@ -51,6 +54,15 @@ class _ProductInformationState extends State<ProductInformation> {
                 new Text("Stock : ${widget.product.stock}", style: TextStyle(fontSize: 18.0),),
                 Padding(padding: EdgeInsets.only(top: 8.0),),
                 Divider(),
+                Container(
+                          height: 300.0,
+                          width: 300.0,
+              child: Center(
+                child: productImage == ''
+                    ? Text('No Image')
+                    : Image.network(productImage+'?alt=media'),//nuevo para traer la imagen de firestore
+              ),
+            ),
               ],
             ),
           ),
